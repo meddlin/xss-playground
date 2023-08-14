5f5b61e70ea8bfb0673f0a04dff5b2f6199c8097
meddlin <drushing.dev@gmail.com>
2023-08-14 16:30:26 -0500

    successfully detect alert from XSS

--- py-selenium/test.py
+++ py-selenium/test.py
@@ -1,8 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 
-def test_eight_components():
+def test_xss():
     driver = webdriver.Chrome()
 
     driver.get("http://localhost:3000")
@@ -15,17 +18,27 @@ def test_eight_components():
     text_box = driver.find_element(by=By.ID, value="input-field")
     submit_button = driver.find_element(by=By.ID, value="btn-click")
 
-    text_box.send_keys("Selenium")
+    text_box.send_keys("<image/src/onerror=prompt(8)>")
     submit_button.click()
 
-    message = driver.find_element(by=By.ID, value="response-display")
-    value = message.text
-    assert value == "Selenium"
+
+    # Xss should happen in here
+    try:
+        WebDriverWait(driver, 3).until(EC.alert_is_present())
+        print("alert is present")
+        print('XSS attack is successful')
+    except TimeoutException:
+        print("No alert is present")
+
+
+    # message = driver.find_element(by=By.ID, value="response-display")
+    # value = message.text
+    # assert value == "Selenium"
 
     driver.quit()
-    print("value is...")
-    print(value)
+    # print("value is...")
+    # print(value)
 
 
 
-test_eight_components()
+test_xss()
